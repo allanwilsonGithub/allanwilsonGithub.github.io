4da6ff86dcd79c9a0e21280e37e6f767fb17e381
Allan Wilson <allan@allanwilson.net>
2018-12-08 23:16:18 +0200

    Added pic toggle and Hold

--- js/alluPigGame/app.js
+++ js/alluPigGame/app.js
@@ -20,11 +20,6 @@ diceSet = 2
 
 
 var x = document.querySelector('#score-0').textContent;
-console.log({x});
-
-function btn(){
-    console.log('Clicked baby!')
-}
 
 document.getElementById('score-0').textContent = 0;
 document.getElementById('score-1').textContent = 0;
@@ -49,29 +44,41 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         document.getElementById('current-' + activePlayer).textContent = roundScore;
         document.querySelector('.dice').style.display = 'block';
     } else {
-        document.querySelector('.dice').style.display = 'block';
-        //TODO add a 1 sec wait here so that the dice with 1 is shown for a bit before disapearing
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-        document.getElementById('current-0').textContent = roundScore;
-        document.getElementById('current-1').textContent = roundScore;
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        document.querySelector('.dice').style.display = 'none';
+        // Next player
+        nextPlayer();
     }
 
 });
 
 document.querySelector('.btn-hold').addEventListener('click',function(){
     //Add current score to player's GLOBAL SCORE
-
+    scores[activePlayer] += roundScore;
     // Update the UI
-
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     // Check if the player has won the game
+
+    // Change active player
+    nextPlayer();
 
     
 })
 
+document.querySelector('.btn-pics').addEventListener('click', function(){
+    diceSet === 1 ? diceSet = 2 : diceSet = 1;
+})
+
+function nextPlayer(){
+    document.querySelector('.dice').style.display = 'block';
+    //TODO add a 1 sec wait here so that the dice with 1 is shown for a bit before disapearing
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = roundScore;
+    document.getElementById('current-1').textContent = roundScore;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'block';
+    document.querySelector('.dice').style.display = 'none';
+}
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
