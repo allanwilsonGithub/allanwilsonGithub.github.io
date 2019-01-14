9d3be9970d6543309d6bebb47dc1ba6c328cbe43
Allan Wilson <allan@allanwilson.net>
2019-01-14 23:26:42 +0200

    .

--- coding/python/oop.py
+++ coding/python/oop.py
@@ -1,3 +1,10 @@
+# A class is a template for creating objects
+# An object is an instance of a class
+# Instantiate: create an instance of a class
+# Method: a function called in a class
+# Attribute: a variable bound to a class instance
+
+
 class Kettle(object):
 
     def __init__(self, make, price):
