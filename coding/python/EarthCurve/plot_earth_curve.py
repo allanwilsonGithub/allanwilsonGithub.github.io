a7d141d0d862ed81b7def53ca6484b15f33e770d
Allan Wilson <allan@allanwilson.net>
2019-01-20 01:52:45 +0200

    Plot Earth Curve For Weather Radar

--- coding/python/EarthCurve/plot_earth_curve.py
+++ coding/python/EarthCurve/plot_earth_curve.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import math
 
 yAxis = []
 xAxis = []
+ground = []
 for i in range(1, 450):
     alt = (math.sqrt((6371 * 6371) + (i * i))) - 6371
     yAxis.append(i)
     xAxis.append(alt)
+    ground.append(0)
 
-# Radar Image
-# image = mpimg.imread('weather_radar.png')
-# plt.imshow(image)
 
 plt.plot(yAxis, xAxis, linewidth=5.0)
 plt.title('Radar Ray starting at 0 altitude')
-plt.text(150, 0.25, r'$\mu=100,\ \sigma=15$')
+plt.text(150, 0.25, r'Ground level')
+plt.text(300, 10, r'Radar Beam')
+plt.plot(yAxis, ground, 'g--')
 plt.ylabel('altitude')
 plt.xlabel('distance from radar (kms)')
 plt.show()
