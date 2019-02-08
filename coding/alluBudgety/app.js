fb44de0bb38f18e982ac08be40a5297fbe8d8746
Allan Wilson <allan@allanwilson.net>
2019-02-09 01:04:58 +0200

    .

--- coding/alluBudgety/app.js
+++ coding/alluBudgety/app.js
@@ -1,3 +1,4 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
     var x = 23;
@@ -14,20 +15,38 @@ var budgetController = (function() {
 
 })();
 
+
+// UI CONTROLLER
 var UIcontroller = (function(){
 
     //some code
 
 })();
 
+
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function(){
 
-    return {
-        anotherPublic: function(){
-            console.log(z);
-        }
+        // 1. Get the field input data
+        
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the user interface
+
+        // 4. Calculate the budget
+
+        // 5. Display te budget on the UI
+ 
     }
 
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    
+    document.addEventListener('keypress', function(event){
+        if (event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        };
+    })
 })(budgetController, UIcontroller);
