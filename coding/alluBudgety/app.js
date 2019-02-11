9a3c9664bfefcb433dca4c90d6a27f336275dcaa
Allan Wilson <allan.wilson@vaisala.com>
2019-02-11 17:27:21 +0200

    .

--- coding/alluBudgety/app.js
+++ coding/alluBudgety/app.js
@@ -19,7 +19,26 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIcontroller = (function(){
 
-    //some code
+    DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+    
+    return {
+        getinput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,   // will be either inc or exp
+                discription: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
 
 })();
 
@@ -28,9 +47,13 @@ var UIcontroller = (function(){
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl){
 
+    var DOM = UICtrl.getDOMstrings();
+    
     var ctrlAddItem = function(){
 
         // 1. Get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
         
         // 2. Add the item to the budget controller
 
@@ -42,7 +65,7 @@ var controller = (function(budgetCtrl, UICtrl){
  
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
     
     document.addEventListener('keypress', function(event){
         if (event.keyCode === 13 || event.which === 13){
