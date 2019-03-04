6d0230a1b44946007109ff05a36379b13fc81857
Allan Wilson <allan@allanwilson.net>
2019-03-04 21:43:15 +0200

    indexof problem

--- coding/alluBudgety/app.js
+++ coding/alluBudgety/app.js
@@ -65,10 +65,26 @@ var budgetController = (function() {
         },
         
         deleteItem: function(type, id) {
-
+            var ids, index;
+            
+            // example of .map
+            // the id we want ... id = 6
+            // data.allItems[type}[id];
+            // all the ids = [1 2 4 6 8]
+            // .map returns a new array replacing each value with the result of the function
             // id = 3
+            
+            ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+            console.log(ids);
+            index = ids.indexof(id);
 
-        }
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+
+        },
 
         calculateBudget: function() {
 
@@ -251,9 +267,10 @@ var controller = (function(budgetCtrl, UICtrl){
         if(itemID){
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             // 1. Delete item from data structure
+            budgetController.deleteItem(type, ID);
 
             // 2. Delete item from the UI
 
