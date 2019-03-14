7b46756feac54be8f280baf3a525ab985bd31206
Allan Wilson <allan@allanwilson.net>
2019-03-14 21:50:02 +0200

    Budgetty percentages added

--- coding/alluBudgety/app.js
+++ coding/alluBudgety/app.js
@@ -165,7 +165,8 @@ var UIcontroller = (function(){
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        expensesPercLabel: '.item__percentage'
     };
     
     return {
@@ -243,6 +244,24 @@ var UIcontroller = (function(){
             
         },
 
+        displayPercentages: function(percentages){
+            var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+            var nodeListForEach = function(list, callback){
+                for (var i = 0; i < list.length; i++){
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fields, function(current, index){
+                if (percentages[index] > 0){
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---';
+                }
+            });
+        },
+         
         getDOMstrings: function(){
             return DOMstrings;
         }
@@ -289,7 +308,7 @@ var controller = (function(budgetCtrl, UICtrl){
         var percentages = budgetCtrl.getPercentages();
 
         // 3. Update the UI with the new percentages
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
     }
 
     var ctrlAddItem = function(){
