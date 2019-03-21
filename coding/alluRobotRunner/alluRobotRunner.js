469894206527bd3ca51b22df5fb3e66f35d630d4
Allan Wilson <allan@allanwilson.net>
2019-03-22 01:21:18 +0200

    looking fecking good!

--- coding/alluRobotRunner/alluRobotRunner.js
+++ coding/alluRobotRunner/alluRobotRunner.js
@@ -1,17 +1,38 @@
-let moveRobotLeft = function () {
-    console.log("Robot moves left");
-}
-let moveRobotUp = function () {
-    console.log("Robot moves up");
-}
-let moveRobotDown = function () {
-    console.log("Robot moves down");
-}
-let moveRobotRight = function () {
-    console.log("Robot moves right");
-}
+//  R O B O T   R U N N E R
 
-document.querySelector('.arrow-button-left').addEventListener('click', moveRobotLeft);
-document.querySelector('.arrow-button-up').addEventListener('click', moveRobotUp);
-document.querySelector('.arrow-button-down').addEventListener('click', moveRobotDown);
-document.querySelector('.arrow-button-right').addEventListener('click', moveRobotRight);
+var robotRunner = (function(){
+
+    var setupEventListeners = function(){
+        document.querySelector('.arrow-button-left').addEventListener('click', moveRobotLeft);
+        document.querySelector('.arrow-button-up').addEventListener('click', moveRobotUp);
+        document.querySelector('.arrow-button-down').addEventListener('click', moveRobotDown);
+        document.querySelector('.arrow-button-right').addEventListener('click', moveRobotRight);
+    };
+ 
+    let moveRobotLeft = function () {
+        console.log("Robot moves left");
+    };
+
+    let moveRobotUp = function () {
+        console.log("Robot moves up");
+    };
+
+    let moveRobotDown = function () {
+        console.log("Robot moves down");
+    };
+
+    let moveRobotRight = function () {
+        console.log("Robot moves right");
+    };
+
+    return {
+        init: function(){
+            console.log('RobotRunner has started...');
+            setupEventListeners();
+            
+        }
+    };
+
+})();
+
+robotRunner.init();
