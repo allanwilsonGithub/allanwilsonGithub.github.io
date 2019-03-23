fefe1c60f1bd55dda4243afb990165031bd17503
Allan Wilson <allan@allanwilson.net>
2019-03-23 02:39:18 +0200

    ssh for robot added

--- coding/alluRobotRunner/py/robotLeft.py
+++ coding/alluRobotRunner/py/robotLeft.py
@@ -1,1 +1,11 @@
+import paramiko
+
 f = open("left.txt","w+")
+
+robot_IP = '192.168.x.x'
+username = 'pi'
+password = 'raspberry'
+
+ssh = paramiko.SSHClient()
+ssh.connect(robot_IP, username=username, password=password)
+ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd_to_execute)
